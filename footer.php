2a6b65ac4d0c97b7f474810957202cc4ec360b40
basarballioz <balliozbasar@gmail.com>
2021-05-18 23:30:03 +0300

    files are added

---
+++ footer.php
@@ -0,0 +1,13 @@
+  <!-- footer -->
+  <footer class="container text-center">
+    <ul class="nav nav-pills pull-right">
+      <li>Socialize - Made by Başar Ballıöz, Aykut Güler | For IT306 PROJECT</li>
+    </ul>
+  </footer>
+  <!-- ./footer -->
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="js/script.js"></script>
+</body>
+</html>
+
+<?php $conn->close(); ?>
